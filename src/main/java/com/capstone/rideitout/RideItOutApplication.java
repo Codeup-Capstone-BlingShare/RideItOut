f69a41429d38f212183780f94bef5c246f3a853f
James OSullivan <james.a.osullivan01@gmail.com>
2023-08-18 14:51:35 -0500

    application.properties added to ignore

---
+++ src/main/java/com/capstone/rideitout/RideItOutApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.rideitout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RideItOutApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RideItOutApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/capstone/rideitout/RideItOutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.rideitout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RideItOutApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

147b51a4e20619278289cfb930ec81ecefcc54aa
James OSullivan <james.a.osullivan01@gmail.com>
2023-08-21 15:40:05 -0500

    Finished most of the the authetication set up, waiting on the rest of the URL mappings.

---
+++ src/main/java/com/capstone/rideitout/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.capstone.rideitout.repositories;
+
+import com.capstone.rideitout.Model.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<Users, Long> {
+        Users findByUsername(String username);
+
+
+}

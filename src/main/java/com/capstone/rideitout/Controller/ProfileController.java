9f787e8d346fac427ead878a4788db2268e5e345
emata10 <130502916+emata10@users.noreply.github.com>
2023-08-21 11:59:53 -0600

    Controllers, profile, login, and profile.html

--- src/main/java/com/capstone/rideitout/Controller/ProfileController.java
+++ src/main/java/com/capstone/rideitout/Controller/ProfileController.java
@@ -1,33 +1,22 @@
 package com.capstone.rideitout.Controller;
+
 import com.capstone.rideitout.Model.Users;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+
+
 @Controller
 public class ProfileController {
 
-    private UserService userService;
-
-    @Autowired
-    public ProfileController(UserService userService) {
-        this.userService = userService;
-    }
-
     @GetMapping("/profile")
     public String showProfilePage(Model model) {
-        Users user = userService.getUser(); // Retrieve user data from the userService
-        model.addAttribute("user", user); // Add user object to the model
+        // Simulate user data retrieval (replace with actual user data retrieval logic)
+        Users user = new Users(1L, "John", "Doe", "johndoe@example.com", "password123");
+
+        // Add user object to the model
+        model.addAttribute("user", user);
+
         return "profile";
     }
-
-    @PostMapping("/save-profile")
-    public String saveProfile(@ModelAttribute Users user) {
-        userService.saveUser(user); // Save profile changes to the database using userService
-        return "redirect:/profile"; // Redirect back to the profile page after saving
-    }
-
-    // Other methods for managing cars and displaying past trips
 }

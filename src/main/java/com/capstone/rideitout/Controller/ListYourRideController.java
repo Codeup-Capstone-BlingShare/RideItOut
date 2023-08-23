be6c2ffd8cf9af94b64b87a7c54fd35ef8c80aef
James OSullivan <james.a.osullivan01@gmail.com>
2023-08-23 11:24:23 -0500

    Finished getting the cars Entity to properly show user_id

--- src/main/java/com/capstone/rideitout/Controller/ListYourRideController.java
+++ src/main/java/com/capstone/rideitout/Controller/ListYourRideController.java
@@ -1,19 +1,30 @@
 package com.capstone.rideitout.Controller;
 
 import com.capstone.rideitout.Model.Car;
+import com.capstone.rideitout.Model.Users;
 import com.capstone.rideitout.repositories.CarRepository;
+import com.capstone.rideitout.repositories.UserRepository;
+import jakarta.persistence.Id;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
 
 
 @Controller
 @RequestMapping("/listyourride")
 public class ListYourRideController {
 
+
+    private UserRepository userRepository;
+
+
     private final CarRepository carDao;
 
-    public ListYourRideController(CarRepository carDao) {
+    public ListYourRideController(UserRepository userRepository, CarRepository carDao) {
+        this.userRepository = userRepository;
         this.carDao = carDao;
     }
 
@@ -26,10 +37,14 @@ public class ListYourRideController {
 
 
     @PostMapping
-    public String addCar(@ModelAttribute Car car) {
+    public String addCar(@ModelAttribute Car car, Principal principal) {
+        Users user = (Users) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        car.setUser(user);
         carDao.save(car);
+
         return "redirect:/manage";
     }
+
 
 
 }

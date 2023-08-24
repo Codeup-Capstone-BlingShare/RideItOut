0960f647325bb4d343305ecf6d4e93b918da8469
James OSullivan <james.a.osullivan01@gmail.com>
2023-08-24 15:37:54 -0500

    Finished getting the user_id to populate to DB

--- src/main/java/com/capstone/rideitout/Controller/ListYourRideController.java
+++ src/main/java/com/capstone/rideitout/Controller/ListYourRideController.java
@@ -1,6 +1,8 @@
 package com.capstone.rideitout.Controller;
 
 import com.capstone.rideitout.Model.Car;
+import com.capstone.rideitout.Model.CarForm;
+import com.capstone.rideitout.Model.Photo;
 import com.capstone.rideitout.Model.Users;
 import com.capstone.rideitout.repositories.CarRepository;
 import com.capstone.rideitout.repositories.UserRepository;
@@ -11,6 +13,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
+import java.util.Collections;
 
 
 @Controller
@@ -30,16 +33,29 @@ public class ListYourRideController {
 
     @GetMapping
     public String listYourRide(Model model) {
-        Car car = new Car();
-        model.addAttribute("car", car);
+        CarForm carForm = new CarForm();
+        model.addAttribute("carForm", carForm);
         return "Users/listyourride";
     }
-
+//, Principal principal
 
     @PostMapping
-    public String addCar(@ModelAttribute Car car, Principal principal) {
+    public String addCar(@ModelAttribute("carForm") CarForm carForm) {
         Users user = (Users) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+//        System.out.println(user.getId());
+
+        user = userRepository.getById(user.getId());
+//        System.out.println(user.getId());
+        Car car = new Car(carForm.getMake(), carForm.getModel(), carForm.getYear(), carForm.getMileage(),
+                carForm.isAvailable(), carForm.getPricePerDay(), carForm.getCarLocationZip());
+
+        Photo photo = new Photo();
+        photo.setCarPhotoURL(carForm.getCarPhotoURL());
+        photo.setCar(car);
+        car.setPhotos(Collections.singletonList(photo));
+
         car.setUser(user);
+        System.out.println(car.getUser().getId());
         carDao.save(car);
 
         return "redirect:/manage";
--- src/main/java/com/capstone/rideitout/Model/Photo.java
+++ src/main/java/com/capstone/rideitout/Model/Photo.java
@@ -1,13 +1,41 @@
 package com.capstone.rideitout.Model;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
 @Table(name = "photos")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Photo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "car_id")
+    private Car car;
+
+
+    @Column(name = "car_photo_url")
+    private String carPhotoURL;
+
+    @Override
+    public String toString() {
+        return "Photo{" +
+                "id=" + id +
+                ", carPhotoURL='" + carPhotoURL + '\'' +
+                // Exclude car field from toString()
+                '}';
+    }
+
+    public Photo(String carPhotoURL) {
+        this.carPhotoURL = carPhotoURL;
+    }
+
+    //request API object
 
 }

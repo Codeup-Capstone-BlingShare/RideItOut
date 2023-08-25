5f0c083c51dd4a5cafea0fef543364b848842081
emata10 <130502916+emata10@users.noreply.github.com>
2023-08-25 16:38:38 -0600

    Edited home.html, carousel.js, style.css

--- src/main/resources/static/js/carousel.js
+++ src/main/resources/static/js/carousel.js
@@ -1,1 +1,18 @@
 $('.carousel').carousel();
+
+function showAnswer() {
+	var dropdown = document.getElementById("questionDropdown");
+	var selectedValue = dropdown.value;
+	var answerContainer = document.getElementById("answerContainer");
+	
+	// Hide all answers
+	var answers = answerContainer.getElementsByClassName("answer");
+	for (var i = 0; i < answers.length; i++) {
+		answers[i].classList.add("hidden");
+	}
+	
+	// Show selected answer
+	if (selectedValue) {
+		document.getElementById(selectedValue).classList.remove("hidden");
+	}
+}

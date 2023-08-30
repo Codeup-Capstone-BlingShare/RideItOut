e49ddf65fed5c918e98dc1a7a9a310a1c80747eb
James OSullivan <james.a.osullivan01@gmail.com>
2023-08-30 15:05:33 -0500

    Added profile picture functionality

--- src/main/java/com/capstone/rideitout/Model/Users.java
+++ src/main/java/com/capstone/rideitout/Model/Users.java
@@ -1,10 +1,14 @@
 package com.capstone.rideitout.Model;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 
 
 @Entity
+@Getter
+@Setter
 @Table(name="users")
 public class Users {
 
@@ -28,6 +32,10 @@ public class Users {
     @Column(name = "password", unique = true)
     private String password;
 
+    @Column(name = "profile_photo_url")
+    private String profilePhoto;
+
+
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "user")
     private List<Car> listedCars;
 
@@ -46,12 +54,12 @@ public class Users {
     public Users() {
     }
 
-    public Users(String email, String firstName, String lastName, String password, String username){
+    public Users(String email, String firstName, String lastName, String password, String username) {
         this.email = email;
         this.username = username;
-        this.firstName= firstName;
-        this.lastName=lastName;
-        this.password=password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.password = password;
     }
 
     public Users(long id, String username, String email, String password) {
@@ -77,60 +85,14 @@ public class Users {
         password = copy.password;
     }
 
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getUsername(){return username;}
-
-    public void setUsername(String username){this.username = username;}
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-
     @Override
     public String toString() {
         return "Users{" +
                 "id=" + id +
                 ", firstName='" + firstName + '\'' +
                 ", lastName='" + lastName + '\'' +
-                ", email='" + email + '\'' +
-                ", password='" + password + '\'' +
+                ", username='" + username + '\'' +
+                ", profilePhoto='" + profilePhoto + '\'' +
                 '}';
     }
 }

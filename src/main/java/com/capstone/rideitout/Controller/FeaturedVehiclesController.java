3176d2a40e19c35efed6bd662ca6ee9b1e5196bd
emata10 <130502916+emata10@users.noreply.github.com>
2023-08-31 15:28:25 -0600

    faq.html

--- src/main/java/com/capstone/rideitout/Controller/FeaturedVehiclesController.java
+++ src/main/java/com/capstone/rideitout/Controller/FeaturedVehiclesController.java
@@ -1,9 +1,5 @@
 package com.capstone.rideitout.Controller;
-
-
-
 import com.capstone.rideitout.Model.Search;
-import com.capstone.rideitout.Model.Vehicle;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;

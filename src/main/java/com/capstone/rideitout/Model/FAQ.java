6f1ad78e8908ca3d4f2844f6f5ee5b6c168fa157
emata10 <130502916+emata10@users.noreply.github.com>
2023-08-30 23:16:54 -0600

    FAQController,FAQ.html,faq.java, and edited style.css

--- src/main/java/com/capstone/rideitout/Model/FAQ.java
+++ src/main/java/com/capstone/rideitout/Model/FAQ.java
@@ -1,25 +1,29 @@
 package com.capstone.rideitout.Model;
 
-import lombok.Getter;
-
-@Getter
 public class FAQ {
     private String question;
     private String answer;
-    public FAQ() {
-
-    }
 
     public FAQ(String question, String answer) {
         this.question = question;
         this.answer = answer;
     }
 
+    // getters and setters
+    public String getQuestion() {
+        return question;
+    }
+
     public void setQuestion(String question) {
         this.question = question;
+    }
+
+    public String getAnswer() {
+        return answer;
     }
 
     public void setAnswer(String answer) {
         this.answer = answer;
     }
+
 }

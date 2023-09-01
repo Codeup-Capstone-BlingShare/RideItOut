c0e0d357c6974bbf5ddd49c86d7979f4df6e2236
Elliot Anderton <elliot.j.anderton@gmail.com>
2023-09-01 13:47:43 -0700

    Created delete photos

--- src/main/java/com/capstone/rideitout/Controller/ManageCarsController.java
+++ src/main/java/com/capstone/rideitout/Controller/ManageCarsController.java
@@ -1,8 +1,10 @@
 package com.capstone.rideitout.Controller;
 
 import com.capstone.rideitout.Model.Car;
+import com.capstone.rideitout.Model.Photo;
 import com.capstone.rideitout.Model.Users;
 import com.capstone.rideitout.repositories.CarRepository;
+import com.capstone.rideitout.repositories.PhotoRepository;
 import com.capstone.rideitout.repositories.UserRepository;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
@@ -10,14 +12,18 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.List;
+
 @Controller
 public class ManageCarsController {
     private final CarRepository carDoa;
     private final UserRepository userDoa;
+    private final PhotoRepository photoDoa;
 
-    public ManageCarsController(CarRepository carDoa, UserRepository userDoa) {
+    public ManageCarsController(CarRepository carDoa, UserRepository userDoa, PhotoRepository photoDoa) {
         this.carDoa = carDoa;
         this.userDoa = userDoa;
+        this.photoDoa = photoDoa;
     }
 
     @GetMapping("/manage")
@@ -42,6 +48,13 @@ public class ManageCarsController {
         car.setUser(user);
         car.setRented(oldCar.isRented());
 
+//        List<Photo> carPhotos = car.getPhotos();
+//        for (Photo carPhoto : carPhotos) {
+//            String url = carPhoto.getCarPhotoURL();
+//            Photo photo = new Photo(car, url);
+//            photoDoa.save(photo);
+//        }
+
         carDoa.save(car);
         return "redirect:/manage";
     }
@@ -50,5 +63,16 @@ public class ManageCarsController {
     public String deleteCar(@PathVariable long id) {
         carDoa.deleteById(id);
         return "redirect:/manage";
+    }
+
+    @PostMapping("/deleteImg/{id}")
+    public String deleteImg(@PathVariable long id, @RequestParam long carid) {
+        Photo deletePhoto = photoDoa.getReferenceById(id);
+        System.out.println("here");
+//        System.out.println(id);
+//        System.out.println(carid);
+//        System.out.println(deletePhoto.getCarPhotoURL());
+        photoDoa.delete(deletePhoto);
+        return "redirect:/car/" + carid + "/edit";
     }
 }

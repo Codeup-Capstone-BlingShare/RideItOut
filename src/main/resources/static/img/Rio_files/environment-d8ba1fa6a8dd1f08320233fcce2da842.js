4e5fda5e3d7db3be48e2ce263f82121cd523f866
emata10 <130502916+emata10@users.noreply.github.com>
2023-09-05 15:36:54 -0600

    deleted rio files

--- src/main/resources/static/img/Rio_files/environment-d8ba1fa6a8dd1f08320233fcce2da842.js
+++
@@ -1,3 +0,0 @@
-define("home/config/environment",(function(){return{default:{modulePrefix:"home",podModulePrefix:"home/pods",rootURL:"/",locationType:"auto",phoneInput:{lazyLoad:!1},"ember-component-css":{terseClassNames:!1}}}}))
-
-//# sourceMappingURL=environment-d8ba1fa6a8dd1f08320233fcce2da842.map
